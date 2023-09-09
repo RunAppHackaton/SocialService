5b29378ef0710ad57f6d06a85e7820dd2f95b483
Denis Durbalov <den.dyrbalov25@gmail.com>
2023-09-09 14:28:57 +0200

    RA-60 add models and other stuff

--- src/main/java/com/runapp/socialservice/SocialServiceApplication.java
+++ src/main/java/com/runapp/socialservice/SocialServiceApplication.java
@@ -1,13 +1,16 @@
 package com.runapp.socialservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SocialServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SocialServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SocialServiceApplication.class, args);
+    }
 
 }
